08fd933adb6c3a88e80df3a6c9739f6c912398c0
Jonny Deitos <jonny@Jonnys-iMac.local>
2023-06-02 14:51:40 -0300

    finalização cors no servidor

--- src/server.js
+++ src/server.js
@@ -10,23 +10,23 @@ const app = express()
 db.connect()
 
 // habilita CORS
-// const allowedOrigins = [
-//   'http://127.0.0.1:5500',
-//   'http://www.app.com.br',
-// ]
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://www.app.com.br',
+]
 
-// app.use(cors({
-//   origin: function(origin, callback) {
-//     // permite requests sem 'origin' - exemplo: mobile apps
-//     if (!origin) return callback(null, true)
+app.use(cors({
+  origin: function(origin, callback) {
+    // permite requests sem 'origin' - exemplo: mobile apps
+    if (!origin) return callback(null, true)
 
-//     // verifica se o 'origin' está na lista de permitidos
-//     if (!allowedOrigins.includes(origin)) return callback(null, false)
+    // verifica se o 'origin' está na lista de permitidos
+    if (!allowedOrigins.includes(origin)) return callback(null, false)
 
-//     // autoriza request
-//     return callback(null, true)
-//   }
-// }))
+    // autoriza request
+    return callback(null, true)
+  }
+}))
 
 // habilita server para receber dados json
 app.use(express.json())
