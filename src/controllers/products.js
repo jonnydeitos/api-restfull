9539fd593758090b8e21470ad1bed59af57de8cd
Jonny Deitos <jonny@Jonnys-iMac.local>
2023-06-02 09:06:26 -0300

    add método get

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,14 +1,12 @@
 const ProductsModel = require('../models/products')
 
 async function get(req, res) {
-  // const { id } = req.params
+  const { id } = req.params
 
-  // const obj = id ? { _id: id } : null
+  const obj = id ? { _id: id } : null
 
-  const products = await ProductsModel.find()
-  // const products = await ProductsModel.find(obj)
+  const products = await ProductsModel.find(obj)
 
-  // res.send(products)
   res.send(products)
 }
 
